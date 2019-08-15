188d9d10eeb90038a5f88290f4e7c9610f553ec6
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-15 08:43:28 +0200

    Add project files.

---
+++ Talrække/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Talrække
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+
+            for (i = 20; i <= 20; i-- )
+            {
+                Console.WriteLine("{0}", i);
+                if (i == 0)
+                {
+                    Console.WriteLine("Færdig");
+                    break;
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
